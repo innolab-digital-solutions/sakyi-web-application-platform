8efcfb6d8cd0c7894c09febd4729baf46430946a
aung-thu-zaw <aungthuzaw223@gmail.com>
2025-08-26 21:44:26 +0630

    SK-10 remove unnecessary hook and adjust use breadcrumbs hook

--- src/hooks/use-title.ts
+++
@@ -1,6 +0,0 @@
-import { useBreadcrumbs } from "@/hooks/use-breadcrumbs";
-
-export function useTitle(): string {
-  const breadcrumbs = useBreadcrumbs();
-  return breadcrumbs[breadcrumbs.length - 1]?.label || "Overview";
-}
