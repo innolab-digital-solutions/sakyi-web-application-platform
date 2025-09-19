a442e7b79f00d4363b50f22943617e1ab7496460
aung-thu-zaw <aungthuzaw223@gmail.com>
2025-09-19 13:18:43 +0000

    SK-5 setup simple import sort plugin in my eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,8 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
@@ -13,8 +15,13 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 
   {
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+    },
+
     rules: {
-      "react/no-children-prop": "off",
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
     },
   },
 
