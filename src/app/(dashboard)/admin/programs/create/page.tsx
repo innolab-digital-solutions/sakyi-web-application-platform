34fffa8c6b520564248927edf56a535cd0b8bfa8
kyawMHtet <kyawmhtet23@gmail.com>
2025-11-23 02:37:45 +0700

    change form in program create

--- src/app/(dashboard)/admin/programs/create/page.tsx
+++ src/app/(dashboard)/admin/programs/create/page.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from "next";
 import Link from "next/link";
 import React from "react";
 
-import TeamForm from "@/components/admin/features/teams/team-form";
+import ProgramFormPage from "@/components/admin/features/programs/program-form";
 import PageHeader from "@/components/admin/shared/page-header";
 import { Button } from "@/components/ui/button";
 import { PATHS } from "@/config/paths";
@@ -34,7 +34,7 @@ export default function CreateProgramPage() {
         }
       />
 
-      <TeamForm />
+      <ProgramFormPage />
     </div>
   );
 }
