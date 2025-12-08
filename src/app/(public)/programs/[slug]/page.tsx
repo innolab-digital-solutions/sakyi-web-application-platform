829dbfd2d8461c99bb1295f72d6b24137dfd3aa9
kyawMHtet <kyawmhtet23@gmail.com>
2025-12-08 14:43:21 +0700

    remove export in renderStars program details

--- src/app/(public)/programs/[slug]/page.tsx
+++ src/app/(public)/programs/[slug]/page.tsx
@@ -24,7 +24,7 @@ import { ENDPOINTS } from "@/config/endpoints";
 import { useRequest } from "@/hooks/use-request";
 import { Program } from "@/types/public/program";
 
-export function renderStars(rating: number) {
+function renderStars(rating: number) {
   const fullStars = Math.floor(rating);
   const hasHalfStar = rating % 1 >= 0.5;
   const emptyStars = 5 - fullStars - (hasHalfStar ? 1 : 0);
