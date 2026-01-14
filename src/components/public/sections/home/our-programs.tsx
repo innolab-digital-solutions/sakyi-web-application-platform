15c5ac22d57fb867b7d30b91f6ddd8db87ca833f
kyawMHtet <kyawmhtet23@gmail.com>
2026-01-14 21:43:30 +0700

    change to img from Image in home programs

--- src/components/public/sections/home/our-programs.tsx
+++ src/components/public/sections/home/our-programs.tsx
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @next/next/no-img-element */
+/* eslint-disable no-commented-code/no-commented-code */
 "use client";
 
 import { ArrowRight, Award } from "lucide-react";
@@ -28,7 +31,7 @@ function ProgramCardLarge({ program }: { program: Program }) {
         <div className="flex h-full flex-col gap-6">
           {/* Image - Top (Full Width) */}
           <div className="group/image relative w-full overflow-hidden rounded-xl bg-slate-100">
-            <Image
+            {/* <Image
               src={thumbnailSource}
               alt={program.title}
               width={1200}
@@ -39,6 +42,11 @@ function ProgramCardLarge({ program }: { program: Program }) {
                 hasThumbnail && !imageError ? "object-cover" : "object-contain bg-gray-100"
               }`}
               onError={() => setImageError(true)}
+            /> */}
+            <img
+              src={program.thumbnail}
+              alt={program.title}
+              className="h-auto w-full transition-transform duration-300 group-hover:scale-105 object-cover"
             />
             {/* Subtle dark overlay that disappears on hover */}
             {hasThumbnail && !imageError && (
@@ -94,7 +102,7 @@ function ProgramCardSmall({ program, index }: { program: Program; index: number 
       <div className="flex h-full flex-col gap-4 lg:flex-row lg:gap-4">
         {/* Image - Top on mobile, Left on desktop */}
         <div className="relative w-full overflow-hidden rounded-xl bg-slate-100 lg:w-1/2 lg:h-full">
-          <Image
+          {/* <Image
             src={thumbnailSource}
             alt={program.title}
             width={400}
@@ -105,6 +113,11 @@ function ProgramCardSmall({ program, index }: { program: Program; index: number 
               hasThumbnail && !imageError ? "object-cover" : "object-contain bg-gray-100"
             }`}
             onError={() => setImageError(true)}
+          /> */}
+          <img
+            src={program.thumbnail}
+            alt={program.title}
+            className="h-full w-full transition-transform duration-300 group-hover:scale-105 object-cover"
           />
           {/* Subtle dark overlay that disappears on hover */}
           {hasThumbnail && !imageError && (
